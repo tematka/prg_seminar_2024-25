7020d56ea5033566e7a9251ab6373c58dcc22a65
tematka <145544654+tematka@users.noreply.github.com>
2025-04-11 07:27:04 +0200

    malovani1

---
+++ malovani2/malovani2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace malovani2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelPaiting = new System.Windows.Forms.Panel();
+            this.trackBarPenWidth = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPenWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPaiting
+            // 
+            this.panelPaiting.BackColor = System.Drawing.Color.White;
+            this.panelPaiting.Location = new System.Drawing.Point(6, 119);
+            this.panelPaiting.Name = "panelPaiting";
+            this.panelPaiting.Size = new System.Drawing.Size(782, 324);
+            this.panelPaiting.TabIndex = 0;
+            this.panelPaiting.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panelPaiting_MouseClick);
+            this.panelPaiting.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPaiting_MouseDown);
+            this.panelPaiting.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPaiting_MouseMove);
+            this.panelPaiting.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPaiting_MouseUp);
+            // 
+            // trackBarPenWidth
+            // 
+            this.trackBarPenWidth.Location = new System.Drawing.Point(21, 24);
+            this.trackBarPenWidth.Maximum = 50;
+            this.trackBarPenWidth.Minimum = 1;
+            this.trackBarPenWidth.Name = "trackBarPenWidth";
+            this.trackBarPenWidth.Size = new System.Drawing.Size(252, 45);
+            this.trackBarPenWidth.TabIndex = 1;
+            this.trackBarPenWidth.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarPenWidth);
+            this.Controls.Add(this.panelPaiting);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarPenWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPaiting;
+        private System.Windows.Forms.TrackBar trackBarPenWidth;
+    }
+}
+
---
+++ malovani2/malovani2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace malovani2
+{
+    public partial class Form1 : Form
+    {
+        int X, Y, lastX, lastY, penWidth;
+        bool penDown;
+
+        private void panelPaiting_MouseDown(object sender, MouseEventArgs e)
+        {
+            X = e.X;
+            Y = e.Y;
+            penDown = true;
+            penWidth = (int)trackBarPenWidth.Value;
+        }
+
+        private void panelPaiting_MouseUp(object sender, MouseEventArgs e)
+        {
+            penDown = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            penDown = false;
+        }
+
+        private void panelPaiting_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics gr = panelPaiting.CreateGraphics();
+            lastX = X;
+            lastY = Y;
+            X = e.X;
+            Y = e.Y;
+            Pen pen = new Pen(Color.Black, penWidth);
+            Brush brush = new SolidBrush(Color.Black);
+            if (penDown == true)
+            {
+                gr.DrawLine(pen, lastX, lastY, X, Y);
+                gr.FillEllipse(brush, X - (penWidth / 2), Y - (penWidth / 2), penWidth, penWidth);
+            }
+        }
+
+        private void panelPaiting_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
